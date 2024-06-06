c05fa2e721dd41b369bc580f05fbc0c9ed7e45bd
Sandeep Pandit <sandeep.pandit.messi.10@gmail.com>
2024-06-06 23:13:45 +0530

    Update heading

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
           to="/"
           className="text-3xl font-bold tracking-tight text-orange-500"
         >
-          MernEats.com
+          HeyFoodie
         </Link>
         <div className="md:hidden">
           <MobileNav />
