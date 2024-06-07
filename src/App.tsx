c88ea88f44f4a0e217429205ad1932e112b215ed
Sandeep Pandit <sandeep.pandit.messi.10@gmail.com>
2024-06-07 20:07:34 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,72 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import HomePage from "./pages/HomePage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 import UserProfilePage from "./pages/UserProfilePage";
 import ProtectedRoute from "./auth/ProtectedRouter";
+import ManageRestaurantPage from "./pages/ManageRestaurantPage";
+// import SearchPage from "./pages/SearchPage";
+// import DetailPage from "./pages/DetailPage";
+// import OrderStatusPage from "./pages/OrderStatusPage";
 
-const App = () => {
+const AppRoutes = () => {
   return (
-    <>
-      <Routes>
-        <Route
-          path="/"
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Layout showHero>
+            <HomePage />
+          </Layout>
+        }
+      />
+      <Route path="/auth-callback" element={<AuthCallbackPage />} />
+      {/* <Route
+        path="/search/:city"
+        element={
+          <Layout showHero={false}>
+            <SearchPage />
+          </Layout>
+        }
+      /> */}
+      {/* <Route
+        path="/detail/:restaurantId"
+        element={
+          <Layout showHero={false}>
+            <DetailPage />
+          </Layout>
+        }
+      /> */}
+      <Route element={<ProtectedRoute />}>
+        {/* <Route
+          path="/order-status"
           element={
-            <Layout showHero={true}>
-              <HomePage />
+            <Layout>
+              <OrderStatusPage />
+            </Layout>
+          }
+        /> */}
+        <Route
+          path="/user-profile"
+          element={
+            <Layout>
+              <UserProfilePage />
             </Layout>
           }
         />
-        <Route path="/auth-callback" element={<AuthCallbackPage />} />
-        <Route element={<ProtectedRoute />}>
-          <Route
-            path="/user-profile"
-            element={<Layout><UserProfilePage /></Layout>}
-          />
-        </Route>
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </>
+        <Route
+          path="/manage-restaurant"
+          element={
+            <Layout>
+              <ManageRestaurantPage />
+            </Layout>
+          }
+        />
+      </Route>
+
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
   );
 };
 
-export default App;
+export default AppRoutes;
---
+++ src/config/restaurant-options-config.ts
@@ -0,0 +1,17 @@
+export const cuisineList = [
+  "Samosas",
+  "Dosa",
+  "Biryani",
+  "Aloo Gobi",
+  "Butter Chicken",
+  "Dal Makhani",
+  "Naan",
+  "Matar Paneer",
+
+  "Noodles",
+
+  "Pasta",
+  "Pizza",
+  "Salads",
+  "Seafood",
+];
