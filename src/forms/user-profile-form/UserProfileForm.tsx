c80ab206eb3cf61306c4b9492a44ce3f2476b292
Sandeep Pandit <sandeep.pandit.messi.10@gmail.com>
2024-06-12 15:45:46 +0530

    update

--- src/forms/user-profile-form/UserProfileForm.tsx
+++ src/forms/user-profile-form/UserProfileForm.tsx
@@ -19,7 +19,7 @@ import { useEffect } from "react";
 const formSchema = z.object({
     email: z.string().optional(),
     name: z.string().min(1, "name is required"),
-    addressLine1: z.string().min(1, "Address Line 1 is required"),
+    addressLine: z.string().min(1, "Address Line 1 is required"),
     city: z.string().min(1, "City is required"),
     country: z.string().min(1, "Country is required"),
 });
